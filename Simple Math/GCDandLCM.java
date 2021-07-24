3b1b4226503bbbc97671f9c0ac9f359a0465c94b
Braydon Wang <braydon.wang@gmail.com>
2021-07-24 13:58:41 -0400

    Create GCDandLCM.java

---
+++ Simple Math/GCDandLCM.java
@@ -0,0 +1,36 @@
+package DMOJ;
+import java.util.*;
+import java.io.*;
+public class GCDAndLCM {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int a = readInt(), b = readInt();
+		int gcd = gcd(a,b);
+		int lcm = a*b / gcd(a,b);
+	}
+	static int gcd(int a, int b) {
+		if (b == 0) return a;
+		return gcd(b, a % b);
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
+}
