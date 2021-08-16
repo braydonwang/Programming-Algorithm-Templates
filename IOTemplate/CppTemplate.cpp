75c9ab1781e3bd9b1fe50e943437dd2756ed5b41
Braydon Wang <braydon.wang@gmail.com>
2021-08-16 15:12:54 -0400

    Create CppTemplate.cpp

---
+++ IOTemplate/CppTemplate.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#define f first
+#define s second
+#define all(vec) begin(vec), end(vec)
+#define pf push_front
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mk make_pair
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ll,int> pli;
+
+}
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//    freopen("input.in", "r", stdin);
+//    freopen("output.out", "w", stdout);
+}
